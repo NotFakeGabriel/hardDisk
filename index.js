aa484216c070e9529a656f73920e94accc1ddb28
NotFakeGabriel <gamorimfaleiros@yahoo.com.br>
2020-05-25 19:37:48 -0300

    Adicionado tabela ordinal

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ import { quantitativaCont, quantitativaDisc } from "./modules/quantitativa.js";
 const btn = document.getElementById("btn");
 btn.addEventListener("click", function criarTabela2() {
   let radio = document.getElementsByName("Qualitativa");
+  let ordem = document.getElementById("ordem").value;
   if (radio[0].checked) {
     qualitativaNom();
   } else if (radio[1].checked) {
+    qualitativaNom(ordem);
   }
 });
-
+const ordinal = document.getElementById("ordinal");
+document
+  .getElementById("radQualitativaOrd")
+  .addEventListener("click", function () {
+    ordinal.classList.remove("hide");
+  });
+document
+  .getElementById("radQualitativaNom")
+  .addEventListener("click", function () {
+    ordinal.classList.add("hide");
+    document.getElementById("ordem").value = "";
+  });
 const btn2 = document.getElementById("btn2");
 btn2.addEventListener("click", function criarTabela() {
   let radio = document.getElementsByName("Quantitativa");
--- modules/qualitativa.js
+++ modules/qualitativa.js
@@ -1,9 +1,8 @@
 //Qualitativa!!!!!!!!!!!!!!!!!
-import { convertNumber } from "./conversor.js";
 import { coluna, table, col1, linha } from "./tables.js";
 import { moda, medianaquali } from "./calcs.js";
 
-function qualitativaNom() {
+function qualitativaNom(ordem) {
   const resul = document.getElementById("resultados1");
   resul.innerHTML = "";
   document.getElementById("table1").innerHTML = "";
@@ -12,7 +11,16 @@ function qualitativaNom() {
   vet.forEach((v, i) => {
     vet[i] = vet[i].toUpperCase();
   });
-  let coluna0 = linha(vet);
+  let coluna0;
+  if (ordem) {
+    coluna0 = ordem.split(" ");
+    coluna0.forEach((v, i) => {
+      coluna0[i] = coluna0[i].toUpperCase();
+    });
+  } else {
+    coluna0 = linha(vet);
+  }
+
   let coluna1 = col1(coluna0, vet);
   let coluna2 = coluna(coluna1);
   let modaV = moda(coluna1, coluna0);
